5743019f64cf563c6add7e3a70ab579af4c3116f
ariel <arirockseo@gmail.com>
2023-05-02 01:18:02 -0300

    commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.getElementById('toggle-menu');
+const navbarMenuLinks = document.getElementById('main-menu')
+
+
+toggleMenu.addEventListener('click', ()=>{
+    navbarMenuLinks.classList.toggle('main-menu--show')
+})
